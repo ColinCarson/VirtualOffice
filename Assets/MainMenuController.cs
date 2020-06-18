d3cce4887175e9146c36f3652dfd81a900fcdb7c
DL-MA <dean.lambert@moodys.com>
2020-06-18 21:29:15 +0100

    title and menu with options

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // mainMenu.SetActive(!mainMenu.activeSelf);
+            mainMenu.SetActive(true);
+        }
+    }
+}
