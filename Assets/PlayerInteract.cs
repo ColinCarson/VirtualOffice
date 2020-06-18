c3dc4cd09e42e8d982998ecdcec507a7842e762c
DL-MA <dean.lambert@moodys.com>
2020-06-18 15:48:46 +0100

    interactive pop ups
    there is a pop up based on player location and the player can press a button to interact

---
+++ Assets/PlayerInteract.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField]
+    Canvas messageCanvas;
+    bool trigger = false;
+
+    void Start()
+    {
+        messageCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)&&trigger)
+        {
+            TurnOnMessage();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        trigger = true;
+    }
+
+    private void TurnOnMessage()
+    {
+        messageCanvas.enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    { 
+        TurnOffMessage();
+        trigger = false;
+  
+    }
+
+    private void TurnOffMessage()
+    {
+        messageCanvas.enabled = false;
+    }
+}
