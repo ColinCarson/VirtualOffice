5ec0e91c9682b7e126475906c9edc04be14362da
DL-MA <dean.lambert@moodys.com>
2020-06-19 18:08:45 +0100

    multiple NPCs and Clubhouse
    the camera follow doesn't work

---
+++ Assets/CameraFollow_Y_AXIS_Only.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow_Y_AXIS_Only : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform player;
+    GameObject gameCamera;
+
+    void Start()
+    {
+        player = GameObject.Find("PLAYER").transform;
+    }
+
+    void Update()
+    {
+        Vector3 playerpos = player.position;
+        playerpos.y = transform.position.y;
+        transform.position = playerpos;
+    }
+}
--- Assets/MessageController.cs
+++ Assets/MessageController.cs
@@ -13,11 +13,45 @@ public class MessageController : MonoBehaviour
     GameObject Player;
     bool trigger;
     string destinationChar;
+    string destinationID;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         trigger = true;
         destinationChar = other.name;
+        destinationID = IDLookUp(destinationChar);
+    }
+
+    private string IDLookUp(string inputName)
+    {
+        string outString= "";
+
+        const string j = "Justin";
+        const string d = "Dean";
+        const string c = "Conrad";
+        const string n = "Neil";
+        const string ch = "Clubhouse";
+
+        switch (inputName)
+        {
+            case j:
+                outString = "U015W8DN5KN";
+                break;
+            case d:
+                outString = "U016DTQ3JP2";
+                break;
+            case c:
+                outString = "U015HAZ632S";
+                break;
+            case n:
+                outString = "U0162MFLDLZ";
+                break;
+            case ch:
+                outString = "#clubhouse";
+                    break;
+        }
+
+        return outString;
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -34,7 +68,8 @@ public class MessageController : MonoBehaviour
     public void onClick()
     {
         var slackLink = new SlackLink.SlackLink();
-        slackLink.SendMessage("U015W8DN5KN", inputText.GetComponent<Text>().text);
+        //slackLink.SendMessage("U015W8DN5KN", inputText.GetComponent<Text>().text);
+        slackLink.SendMessage(destinationID, inputText.GetComponent<Text>().text);
     }
     // Update is called once per frame
     void Update()
@@ -42,7 +77,8 @@ public class MessageController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C) && trigger)
         {
             var slackLink = new SlackLink.SlackLink();
-            slackLink.StartZoomCall("U015W8DN5KN", "@U015W8DN5KN", "Fancy a video call?", "https://us04web.zoom.us/j/3481207951?pwd=S2VINDFNTUlVbHVFYmNsVXBlZ3pvUT09");
+            //slackLink.StartZoomCall("U015W8DN5KN", "@U015W8DN5KN", "Fancy a video call?", "https://us04web.zoom.us/j/3481207951?pwd=S2VINDFNTUlVbHVFYmNsVXBlZ3pvUT09");
+            slackLink.StartZoomCall(destinationID, "@"+destinationID, "Fancy a video call?", "https://us04web.zoom.us/j/3481207951?pwd=S2VINDFNTUlVbHVFYmNsVXBlZ3pvUT09");
         }
     }
 }
